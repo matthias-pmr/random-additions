3259d584ed17b4db431dad4f670d95a89df51c25
matthias-pmr <matthias@pommeray.eu>
2024-05-21 16:57:01 +0200

    Added initial Bukkit files

---
+++ src/main/java/eu/pommeray/random_additions/Random_additions.java
@@ -0,0 +1,17 @@
+package eu.pommeray.random_additions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Random_additions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
