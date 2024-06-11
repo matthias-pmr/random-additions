8a4ffd8ce46d2c7038756d648fc87cc90f55e2ac
matthias-pmr <matthias@pommeray.eu>
2024-06-11 17:31:58 +0200

    Finished Calculator command and advancement kit event.

--- src/main/java/eu/pommeray/random_additions/RandomAdditions.java
+++ src/main/java/eu/pommeray/random_additions/RandomAdditions.java
@@ -1,8 +1,16 @@
 package eu.pommeray.random_additions;
 import eu.pommeray.random_additions.commands.Calculator;
+import org.bukkit.Instrument;
+import org.bukkit.Material;
+import org.bukkit.Note;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,5 +31,26 @@ public final class RandomAdditions extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onAch(PlayerAdvancementDoneEvent event){
+        Player player = event.getPlayer();
+        Advancement advancement = event.getAdvancement();
+        String name = advancement.getKey().getKey();
+        if(name.equalsIgnoreCase("minecraft:adventure/kill_a_mob")) {
+            player.playNote(player.getLocation(), Instrument.PIANO, Note.natural(1, Note.Tone.A));
+            player.playNote(player.getLocation(), Instrument.BASS_GUITAR, Note.natural(1, Note.Tone.A));
+            player.sendMessage("§8§m-----------------------------------------");
+            player.sendMessage("§a§lCONGRATS!");
+            player.sendMessage("§fYou killed your first §bMob §f!");
+            player.sendMessage("§8§m-----------------------------------------");
+            PlayerInventory inventory = player.getInventory();
+            ItemStack IS = new ItemStack(Material.DIAMOND_SWORD);
+            inventory.addItem();
+            player.sendMessage("§8§m-----------------------------------------");
+            player.sendMessage("§a§lYou were given a Diamond Sword, check your Inventory!");
+            player.sendMessage("§8§m-----------------------------------------");
+        }
     }
 }
--- src/main/java/eu/pommeray/random_additions/commands/Calculator.java
+++ src/main/java/eu/pommeray/random_additions/commands/Calculator.java
@@ -9,10 +9,37 @@ public class Calculator implements CommandExecutor {
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        boolean worked = true;
         if (args.length == 0 || args.length > 3) {
-            return false;
+            worked = false;
         }
-        return true;
+        int firstNumber = 0;
+        int secondNumber = 0;
+        String operation = args[1];
+        try {
+            firstNumber = Integer.parseInt(args[0]);
+            secondNumber = Integer.parseInt(args[2]);
+        }
+        catch (NumberFormatException e) {
+            worked = false;
+        }
+        switch(operation) {
+            case "+" : {
+                sender.sendMessage("The result of " + firstNumber + " + " + secondNumber + " is: " + firstNumber+secondNumber);
+            }
+            case "*" : {
+                sender.sendMessage("The result of " + firstNumber + " * " + secondNumber + " is: " + firstNumber*secondNumber);
+            }
+            case "/" : {
+                sender.sendMessage("The result of " + firstNumber + " / " + secondNumber + " is: " + firstNumber/secondNumber);
+
+            }
+            default : {
+                worked = false;
+            }
+        }
+        return worked;
+
     }
 
 }
