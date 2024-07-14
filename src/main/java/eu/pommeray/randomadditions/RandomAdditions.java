f9598190762c0547e863ed87399cbb1b84a5d7c4
Clara P <clara.57145@gmail.com>
2024-07-14 15:04:11 +0200

    adding some information into the maven site to be more precise

--- src/main/java/eu/pommeray/randomadditions/RandomAdditions.java
+++ src/main/java/eu/pommeray/randomadditions/RandomAdditions.java
@@ -10,14 +10,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
-* The basic class: contains all the methods that are called during the game.
-*/
+/** The basic class: contains all the methods that are called during the game. */
 public class RandomAdditions extends JavaPlugin implements Listener {
 
-  /**
-  * This method is called when the plugin is enabled.
-  */
+  /** This method is called when the plugin is enabled. */
   @Override
   public void onEnable() {
     PluginManager pm = getServer().getPluginManager();
@@ -26,33 +22,31 @@ public class RandomAdditions extends JavaPlugin implements Listener {
     pm.registerEvents(new PoisonousPotatoConsumptionRandomTeleport(), this);
     try {
       Objects.requireNonNull(this.getCommand("calculator"))
-              .setExecutor(new Calculator(this.getLogger()));
+          .setExecutor(new Calculator(this.getLogger()));
     } catch (NullPointerException e) {
-      getLogger().log(java.util.logging.Level.SEVERE,
-              "Could not register the command calculator", e);
+      getLogger()
+          .log(java.util.logging.Level.SEVERE, "Could not register the command calculator", e);
     }
   }
 
   /**
-  * This method is called when a player joins the server.
-  *
-  * @param event the event that is triggered when a player joins the server
-  */
+   * This method is called when a player joins the server.
+   *
+   * @param event the event that is triggered when a player joins the server
+   */
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent event) {
-    event.getPlayer().sendMessage("Hello " + event.getPlayer().getName()
+    event
+        .getPlayer()
+        .sendMessage(
+            "Hello "
+                + event.getPlayer().getName()
                 + ", this is a message proving that the plugin successfully loads!");
   }
 
-  /**
-   * This method is only used if the plugin should be disabled.
-   */
+  /** This method is only used if the plugin should be disabled. */
   @Override
   public void onDisable() {
     // Plugin shutdown logic
   }
-
-
-
-
 }
